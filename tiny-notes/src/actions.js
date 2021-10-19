1917cd512d8b9431084f04acb80b91d13319beb1
sdkag <senyo.k.agawu@gmail.com>
2021-10-18 20:16:38 -0400

    simplify localStorage db

--- tiny-notes/src/actions.js
+++ tiny-notes/src/actions.js
@@ -1,30 +1,23 @@
 // import * as promises from "fs";
-import db from "./notes.json";
+import { notes } from "./notes.json";
 
 export const updateDB = async (writeableJSON) => {
   if (typeof writeableJSON !== "string") {
     writeableJSON = JSON.stringify(writeableJSON);
   }
-  // console.log(promises);
-  // localStorage.db = writeableJSON;
-  // return promises
-  //   .writeFile("./notes.json", JSON.stringify(writeableJSON), "utf8")
-  //   .then((what) => console.log(what))
-  //   .catch((err) => console.log(err));
 
-  localStorage.setItem("db", writeableJSON);
-  console.log(localStorage);
+  localStorage.setItem("notes", writeableJSON);
 };
 
-export const initializeDB = () => updateDB(db);
+export const initializeDB = () => updateDB(notes);
 // fs.writeFile;
 export const deleteNote = (noteId) => {
   if (!noteId) throw Error("noteId is not defined");
-  const notes = JSON.parse(localStorage.getItem("db")).notes.filter(
+  const notes = JSON.parse(localStorage.getItem("notes")).filter(
     (note) => note.id !== noteId
   );
-  updateDB(JSON.stringify({ notes }));
+  updateDB(notes);
   return noteId;
 };
 
-export const fetchNotes = () => JSON.parse(localStorage.getItem("db")).notes;
+export const fetchNotes = () => JSON.parse(localStorage.getItem("notes"));
